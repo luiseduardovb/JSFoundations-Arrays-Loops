dd4ca3ff564cb92bd4019870f5ed75085b6f6a7a
Luis E.Velasquez <luiseduardo@Luiss-MacBook-Pro.local>
2020-06-17 20:28:23 +0300

    task arrayFunctions

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,8 +9,17 @@
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
 function getOdds(numbers) {
-  // Your code here
+  const oddNum = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 1) {
+      oddNum.push(numbers[i]);
+    }
+  }
+
+  return oddNum;
 }
+
+console.log(getOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 /**
  * getEvens(numbers):
@@ -23,8 +32,16 @@ function getOdds(numbers) {
  * getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [52, 14, 56, 888]
  */
 function getEvens(numbers) {
-  // Your code here
+  const evenNum = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNum.push(numbers[i]);
+    }
+  }
+  return evenNum;
 }
+
+console.log([11, 35, 52, 14, 56, 601, 777, 888, 999]);
 
 /**
  * getDuplicateCount(x, numbers):
@@ -36,7 +53,13 @@ function getEvens(numbers) {
  * getDuplicateCount(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
 function getDuplicateCount(x, numbers) {
-  // Your code here
+  let numCount = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (x === numbers[i]) {
+      numCount++;
+    }
+  }
+  return numCount;
 }
 
 /**
@@ -54,7 +77,8 @@ function getDuplicateCount(x, numbers) {
  * - Use string method .endsWith()
  */
 function youGottaCalmDown(s) {
-  // Your code here
+  while (s.endsWith("!!")) s = s.slice(0, -1);
+  return s;
 }
 
 module.exports = { getOdds, getEvens, getDuplicateCount, youGottaCalmDown };
